0a50a87b8f7d0d7205e432da9d08d78c0d782cc7
Mohit Varma <mohit.varma@appfire.com>
2024-02-03 03:37:07 +0530

    Added social links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,14 @@
+// Page Headings
+export const WORK_EXPERIENCE_HEADING = 'Work Experience';
+export const PROJECTS_HEADING = 'Projects';
+export const CODING_PROFILE_HEADING = 'Coding Profiles';
+export const ACCOMPLISHMENTS_HEADING = 'Accomplishments';
+
+// Footer
 export const FOOTER_TRADEMARK_SYMBOL = '© ';
 export const FOOTER_TRADEMARK_TEXT = ' Mohit Varma';
-export const TOTAL_TECH_STACK_CARDS = 4;
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/mohitvarma122/';
+export const GITHUB_URL = 'https://github.com/n0IQ';
+export const TWITTER_URL = 'https://twitter.com/Mohit_Varma1';
+export const CV_URL = 'https://www.linkedin.com/in/mohitvarma122/'; // TODO:
+export const EMAIL_ADDRESS = 'varmamohit122@gmail.com';
