eca29d1e711482145336afa6cd0d52426b0c9d38
Mohit Varma <mohit.varma@appfire.com>
2024-02-04 22:00:12 +0530

    Created big project card

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,10 @@
+import BigProjectCard from '@/components/Project/BigProjectCard';
+
 export default function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="mt-32 mx-auto max-w-screen-xl">
+      <h1 className="text-4xl font-bold font-subHeading-800">My Projects</h1>
+      <BigProjectCard />
     </div>
   );
 }
