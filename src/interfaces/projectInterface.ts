25a06c63f5402fa74e1b4f72d42e603cd634a27d
Mohit Varma <mohit.varma@appfire.com>
2024-02-08 23:07:19 +0530

    created project interface

---
+++ src/interfaces/projectInterface.ts
@@ -0,0 +1,71 @@
+export interface ProjectInterface {
+  title: string;
+  description: string;
+  githubLink: string;
+  youtubeLink: string;
+  projectLink: string;
+}
+
+export const codeNitrixProject: ProjectInterface = {
+  title: 'CodeNitrix - A Coding Assessment Platform',
+  description: '',
+  githubLink: 'https://github.com/n0IQ/CodeNitrix-Backend',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const tripBudsProject: ProjectInterface = {
+  title: 'TripBuds',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const instaPinProject: ProjectInterface = {
+  title: 'InstaPin',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const urlShortenerProject: ProjectInterface = {
+  title: 'URL-Shortener',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const toDoProject: ProjectInterface = {
+  title: 'ToDo-App',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const cryptoTrackerProject: ProjectInterface = {
+  title: 'Crypto Tracker',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const myPortfolioProject: ProjectInterface = {
+  title: 'My Portfolio',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
+
+export const jsDomGameProject: ProjectInterface = {
+  title: 'JS Dom Game',
+  description: '',
+  githubLink: '',
+  youtubeLink: '',
+  projectLink: '',
+};
