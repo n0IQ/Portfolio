a9fd86b45ce5c26ad0c7b2d40bc0bfdad710b8da
Mohit Varma <mohit.varma@appfire.com>
2024-02-10 02:39:47 +0530

    Added project relevant details

--- src/interfaces/projectInterface.ts
+++ src/interfaces/projectInterface.ts
@@ -8,64 +8,83 @@ export interface ProjectInterface {
 
 export const codeNitrixProject: ProjectInterface = {
   title: 'CodeNitrix - A Coding Assessment Platform',
-  description: '',
+  description: `
+  CodeNitrix is a comprehensive online coding assessment platform designed to simplify the hiring process for companies. 
+  It offers a user-friendly interface for companies to create and customize coding tests, set specific questions, and define time limits. 
+  Candidates gain access to these tests through secure passwords provided by the hosting company. With support for JavaScript, engineers 
+  can showcase their coding skills, earning points based on the accuracy of their solutions. CodeNitrix streamlines the assessment process, 
+  providing a seamless experience for both companies and candidates alike.`,
   githubLink: 'https://github.com/n0IQ/CodeNitrix-Backend',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://code-nitrix-frontend.vercel.app/',
 };
 
 export const tripBudsProject: ProjectInterface = {
   title: 'TripBuds',
-  description: '',
-  githubLink: '',
+  description: `TripBuds is a tour booking platform that provides developers with an efficient solution for integrating tour booking 
+  functionality into their applications. With TripBuds, developers can seamlessly incorporate features such as user account creation, 
+  tour booking, and payment processing using Stripe's API. The platform also offers a user-friendly interface with integrated map 
+  functionality, allowing users to visualize tour locations directly within the app. By simplifying the process of integrating tour booking 
+  capabilities, TripBuds enables developers to enhance their applications and provide users with a seamless booking experience.`,
+  githubLink: 'https://github.com/n0IQ/TripBuds',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://trip-buds.vercel.app/',
 };
 
 export const instaPinProject: ProjectInterface = {
   title: 'InstaPin',
-  description: '',
-  githubLink: '',
+  description: `Welcome to InstaPin, an Image Sharing Platform! Instapin is designed to allow users 
+  to share and discover images, create pins, and link them to websites. Users can log in, upload images, 
+  provide short descriptions, and save pins from other users.`,
+  githubLink: 'https://github.com/n0IQ/InstaPin-Backend',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://insta-pin-frontend.vercel.app/',
 };
 
 export const urlShortenerProject: ProjectInterface = {
   title: 'URL-Shortener',
-  description: '',
-  githubLink: '',
+  description: `
+  UrlShortener is a web application designed to efficiently shorten lengthy URLs. It generates unique, random codes for each original link, 
+  creating corresponding short URLs. Users can easily input their long URLs and receive shortened versions, optimizing their sharing 
+  experience. Each short URL seamlessly redirects to its original counterpart, ensuring smooth navigation. Simplify your links with UrlShortener.`,
+  githubLink: 'https://github.com/n0IQ/URL-Shortener',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://url-shortener-five.vercel.app/',
 };
 
 export const toDoProject: ProjectInterface = {
   title: 'ToDo-App',
-  description: '',
-  githubLink: '',
+  description: `This is a simple Todo application built using React, Tailwind CSS, HTML, and Firebase. It allows users to create, read, 
+  update, and delete Todo items. The application uses Firebase Firestore as the database to store and retrieve the Todo data.`,
+  githubLink: 'https://github.com/n0IQ/ToDo-App',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://to-do-app-n0iq.vercel.app/',
 };
 
 export const cryptoTrackerProject: ProjectInterface = {
   title: 'Crypto Tracker',
-  description: '',
-  githubLink: '',
+  description: `Developed a dynamic crypto tracker application using React, enabling users to monitor real-time data for various 
+  cryptocurrencies including Bitcoin. Users can easily track prices, price changes, market capitalization, and more, all in real-time.`,
+  githubLink: 'https://github.com/n0IQ/Crypto-Tracker',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://crypto-tracker-n0iq.vercel.app/',
 };
 
 export const myPortfolioProject: ProjectInterface = {
   title: 'My Portfolio',
-  description: '',
-  githubLink: '',
+  description: `Created a portfolio website to showcase my journey as a software engineer. This platform highlights my skills, 
+  work experience, and various projects, offering insights into my professional background and accomplishments.`,
+  githubLink: 'https://github.com/n0IQ/Portfolio',
   youtubeLink: '',
   projectLink: '',
 };
 
 export const jsDomGameProject: ProjectInterface = {
   title: 'JS Dom Game',
-  description: '',
-  githubLink: '',
+  description: `Created a simple yet engaging game using HTML, CSS, and vanilla JavaScript. This two-player dice game allows users 
+  to take turns rolling a die and accumulating points. Players must strategize when to hold or continue rolling, as rolling a 1 
+  resets their round score. The game ends when one player reaches 100 points.`,
+  githubLink: 'https://github.com/n0IQ/JS-DOM-Game',
   youtubeLink: '',
-  projectLink: '',
+  projectLink: 'https://js-dom-game.vercel.app/',
 };
