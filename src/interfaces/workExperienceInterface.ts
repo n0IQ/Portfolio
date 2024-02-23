1305884638440a323e643b78937db8d842c580d5
Mohit Varma <mohit.varma@appfire.com>
2024-02-24 00:56:54 +0530

    Modified FTE Work Experience

--- src/interfaces/workExperienceInterface.ts
+++ src/interfaces/workExperienceInterface.ts
@@ -11,9 +11,10 @@ const appfireFTEWorkExperience: WorkExperienceInterface = {
   role: 'Associate Software Engineer',
   timeline: 'October 2023 - Present',
   description: [
-    'Contributed to the development of frontend components by utilizing React and Atlaskit library to create user interfaces for cloud applications.',
-    'Constructed backend functionality for cloud applications using the Atlassian Connect framework with Node.js and Express.js.',
-    'Resolved critical bugs in a data center app, improving its stability and reliability.',
+    `Developed and maintained RESTful APIs using Java, adhering to best practices for designing and implementing scalable and reliable APIs.`,
+    `Collaborated with the support team to resolve bug fixes and incorporate customer feature requests into cloud and data center applications.`,
+    `Contributed to the development of unit and integration tests using Gherkin and Cucumber-JS, ensuring code quality and adherence to testing 
+    standards.`,
   ],
   link: 'https://appfire.com/',
 };
